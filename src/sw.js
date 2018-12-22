55c62052b2fc1b395776f5bc73402f644f5a73d9
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2018-12-22 15:13:38 +0100

    event listeners in sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,21 @@
-console.log('In sw.js');
+// console.log('In sw.js');
 
 workbox.skipWaiting();
 workbox.clientsClaim();
+
+self.addEventListener('install', event => {
+  const asyncInstall = new Promise(resolve => {
+    console.log('Waiting to resolve...');
+    setTimeout(resolve, 1500);
+  });
+
+  // console.log('install');
+  event.waitUntil(asyncInstall);
+});
+
+self.addEventListener('activate', event => {
+  console.log('activate');
+});
+
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
